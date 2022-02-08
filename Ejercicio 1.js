1b50554b48faf3ea1f469c4b6af95752d0b9a9d1
Charles-Maddox <68479762+Charles-Maddox@users.noreply.github.com>
2022-02-07 18:40:35 -0600

    Add files via upload

---
+++ Ejercicio 1.js
@@ -0,0 +1,23 @@
+var mes = 10;
+
+if (mes == 12 || mes == 2){
+   console.log("es invierno");
+}else if (mes) {
+   console.log("");
+}
+if (mes == 3 || mes == 5){
+   console.log("es primavera");
+}else if (mes) {
+   console.log("");
+}
+if (mes == 6 || mes == 8){
+   console.log("es verano");
+}else if (mes) {
+   console.log("");
+}
+if (mes == 9 || mes == 11){
+   console.log("es otono");
+}else if (mes) {
+   console.log("");
+}console.log("Gian Carlo Figueroa Diaz ")
+console.log("20000102")
